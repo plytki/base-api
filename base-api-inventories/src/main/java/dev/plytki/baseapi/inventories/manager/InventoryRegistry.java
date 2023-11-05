d88e0c4c1d6dc9fe8a60f5891909e30bfc49f252
Paweł Cichocki <53957038+plytki@users.noreply.github.com>
2023-11-05 19:39:49 +0100

    inventory tracker fix

--- base-api-inventories/src/main/java/dev/plytki/baseapi/inventories/manager/InventoryRegistry.java
+++ base-api-inventories/src/main/java/dev/plytki/baseapi/inventories/manager/InventoryRegistry.java
@@ -23,7 +23,7 @@ public class InventoryRegistry implements Listener {
 
     public void trackInventory(BaseInventory baseInventory, UUID viewer) {
         Deque<BaseInventory> viewerInventories = inventoryTracker.computeIfAbsent(viewer, key -> new ArrayDeque<>());
-        if (viewerInventories.element().getClass().equals(baseInventory.getClass()))
+        if (!viewerInventories.isEmpty() && viewerInventories.element().getClass().equals(baseInventory.getClass()))
             return;
         viewerInventories.push(baseInventory);
     }
